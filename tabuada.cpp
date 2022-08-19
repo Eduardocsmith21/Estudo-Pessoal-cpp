e010ea73f01c20cca851ec480cb67b27b8af1b12
Eduardo Carvalho Smith <eduardocsmith021@gmail.com>
2022-08-18 22:12:02 -0300

    Create tabuada.cpp

---
+++ tabuada.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	
+	int numero;
+	cout << "Digite um numero para saber a tabuada: ";
+	cin >> numero;
+	
+	//Resultado da soma 
+	
+	cout << numero << " X 1 = " << numero * 1 << endl;
+	cout << numero << " X 2 = " << numero * 2 << endl;
+	cout << numero << " X 3 = " << numero * 3 << endl;
+	cout << numero << " X 4 = " << numero * 4 << endl;
+	cout << numero << " X 5 = " << numero * 5 << endl;
+	cout << numero << " X 6 = " << numero * 6 << endl;
+	cout << numero << " X 7 = " << numero * 7 << endl;
+	cout << numero << " X 8 = " << numero * 8 << endl;
+	cout << numero << " X 9 = " << numero * 9 << endl;
+	cout << numero << " X 10 = " << numero * 10 << endl;
+	
+	return 0;
+}
